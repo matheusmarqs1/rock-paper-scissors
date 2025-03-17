583e348b64201b23f4964da4769f2a1e67a0ed9b
Matheus Teles <matheusti770@gmail.com>
2025-03-17 15:25:50 -0300

    Modifying the logic for obtaining user choices.
    Added event listeners to buttons to call
    the playRound function with the correct
    playerSelection on each click.
    Improved CSS for better presentation and
    refactored the code by removing unnecessary
    elements, including the getHumanChoice function.

--- javascript.js
+++ javascript.js
@@ -2,84 +2,67 @@
 /*--------------------                  ROCK, PAPER, SCISSORS           --------------------                                      */
 
 
-//Function with logic to obtain the choice of the computer
+// Trigger the event listener for each button, ensuring that the event handler called uses the specific button that was clicked.
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => playRound(button.id));
+});
+
+// By using the Math.random method, we can generate random numbers between 0 and 1 and divide them into predefined intervals. 
+// Each interval corresponds to a specific range of values, representing one of the computer's three possible choices.
+
 function getComputerChoice(){
     if(Math.random() <= (1/3)){
-        return "Rock";
+        return "rock";
     }
     else if(Math.random() <= (2/3)){
-        return "Paper";
+        return "paper";
     }
     else{
-        return "Scissors";
+        return "scissors";
     }
 }
 
-//function with logic to obtain the choice of the user
-function getHumanChoice(choice){
-    choice = choice.toLowerCase();
-    if(choice === "rock"){
-        return "Rock";
-    }
-    else if(choice === "paper"){
-        return "Paper";
-    }
-    else if(choice === "scissors"){
-        return "Scissors";
-    }
-    else{
-        return "Invalid choice"
-    }
-}
-
-// function that encapsulates the general logic of the game
-function playGame(){
+let humanScore = 0;
+let computerScore = 0;
     
-    // defining scoring variables
-    let humanScore = 0;
-    let computerScore = 0;
+// Using the user's choice captured by the event listener, we define a function that simulates a round of the game. 
+// This function compares the user's selection with the computer's choice and determines the outcome for that round.
+function playRound(humanSelection){
     
-    // function that encapsulates the logic of a single round
-    function playRound(){
-        let choice = prompt("Rock, paper, scissors");
-        // obtains data from the functions responsible for dealing with choices
-        let computerSelection = getComputerChoice();
-        let humanSelection = getHumanChoice(choice);
+    
+    let computerSelection = getComputerChoice();
 
-        // test all scenarios of a rock, paper, scissors game and update  scores
-        if((computerSelection === "Paper" && humanSelection === "Rock" ) || (computerSelection === "Rock" && humanSelection === "Scissors") 
-            || (computerSelection === "Scissors" && humanSelection === "Paper")){
-                console.log("You lose! " + `${computerSelection}` + " beats " + `${humanSelection}`);
-                computerScore++;
-        }
+    if((computerSelection === "paper" && humanSelection === "rock" ) || (computerSelection === "rock" && humanSelection === "scissors") 
+        || (computerSelection === "scissors" && humanSelection === "paper")){
+            console.log("You lose! " + `${computerSelection}` + " beats " + `${humanSelection}`);
+            computerScore++;
+    }
         
-        else if((computerSelection === "Rock" && humanSelection === "Paper" ) || (computerSelection === "Scissors" && humanSelection === "Rock") 
-            || (computerSelection === "Paper" && humanSelection === "Scissors") ){
-                    console.log("You win! " + `${humanSelection}` + " beats " +  `${computerSelection}`);
-                    humanScore++;
-        }
+    else if((computerSelection === "rock" && humanSelection === "paper" ) || (computerSelection === "scissors" && humanSelection === "rock") 
+        || (computerSelection === "paper" && humanSelection === "scissors") ){
+                console.log("You win! " + `${humanSelection}` + " beats " +  `${computerSelection}`);
+                humanScore++;
+    }
        
-        else if(computerSelection === humanSelection){
-            console.log("It's a tie! " + `${computerSelection}` + " against " + `${humanSelection}`);
-        }
-        else{
-            console.log("Invalid round!");
-        }
+    else if(computerSelection === humanSelection){
+        console.log("It's a tie! " + `${computerSelection}` + " against " + `${humanSelection}`);
+    }
         
-    }
-    
-    
-    // compare scores and evaluate the winner
-    if(humanScore > computerScore){
-        console.log("Congrats! You win!")
-    }
-    else if(humanScore < computerScore){
-        console.log("Oh no! You lose!");
-    }
     else{
-        console.log("WOW! Tie Game!");
+        console.log("Invalid round!");
     }
+        
 }
 
-playGame();
 
+if(humanScore > computerScore){
+    console.log("Congrats! You win!")
+}
+else if(humanScore < computerScore){
+    console.log("Oh no! You lose!");
+}
+else{
+    console.log("WOW! Tie Game!");
+}
