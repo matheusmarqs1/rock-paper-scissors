25006aeb3ac0968cdbdbbdee07c4cbb2b4909b6d
Matheus Teles <matheusti770@gmail.com>
2025-03-17 18:22:32 -0300

    Add Game Logic to Continue Until Score Reaches 5.
    Implemented the game logic to keep the game
    running until either the user's or computer's
    score reaches 5.

--- javascript.js
+++ javascript.js
@@ -11,6 +11,7 @@ buttons.forEach((button) => {
 
 let result = document.querySelector(".results");
 
+
 // By using the Math.random method, we can generate random numbers between 0 and 1 and divide them into predefined intervals. 
 // Each interval corresponds to a specific range of values, representing one of the computer's three possible choices.
 
@@ -27,38 +28,46 @@ function getComputerChoice(){
 }
 
 let humanScore = 0;
-let computerScore = 0;
+let computerScore = 0;  
+
     
 // Using the user's choice captured by the event listener, we define a function that simulates a round of the game. 
 // This function compares the user's selection with the computer's choice and determines the outcome for that round.
 function playRound(humanSelection){
-    
-    
-    let computerSelection = getComputerChoice();
-    
-    if((computerSelection === "paper" && humanSelection === "rock" ) || (computerSelection === "rock" && humanSelection === "scissors") 
-        || (computerSelection === "scissors" && humanSelection === "paper")){
-            result.textContent = "You lose! " + `${computerSelection}` + " beats " + `${humanSelection}`;
-            computerScore++;
+    if(humanScore == 5 || computerScore == 5){
+        buttons.forEach((button) => {
+            button.removeEventListener("click", () => playRound(button.id));
+        });
+        return;
     }
-        
-    else if((computerSelection === "rock" && humanSelection === "paper" ) || (computerSelection === "scissors" && humanSelection === "rock") 
-        || (computerSelection === "paper" && humanSelection === "scissors") ){
-                result.textContent = "You win! " + `${humanSelection}` + " beats " +  `${computerSelection}`;
-                humanScore++;
-    }
-       
-    else if(computerSelection === humanSelection){
-        result.textContent = "It's a tie! " + `${computerSelection}` + " against " + `${humanSelection}`;
-    }
-
     else{
-        result.textContent = "Invalid round!";
+        let computerSelection = getComputerChoice();
+    
+        if((computerSelection === "paper" && humanSelection === "rock" ) || (computerSelection === "rock" && humanSelection === "scissors") 
+            || (computerSelection === "scissors" && humanSelection === "paper")){
+                result.textContent = "You lose! " + `${computerSelection}` + " beats " + `${humanSelection}`;
+                ++computerScore;
+                
+        }
+            
+        else if((computerSelection === "rock" && humanSelection === "paper" ) || (computerSelection === "scissors" && humanSelection === "rock") 
+            || (computerSelection === "paper" && humanSelection === "scissors") ){
+                    result.textContent = "You win! " + `${humanSelection}` + " beats " +  `${computerSelection}`;
+                    ++humanScore;
+                    
+        }
+        
+        else if(computerSelection === humanSelection){
+            result.textContent = "It's a tie! " + `${computerSelection}` + " against " + `${humanSelection}`;
+        }
+
+        else{
+            result.textContent = "Invalid round!";
+        }
     }
+    
         
 }
-
-
 
 
 /*
