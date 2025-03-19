c55cc4cbfb699191febc12ab28e6db5426cd508e
Matheus Teles <matheusti770@gmail.com>
2025-03-19 17:34:16 -0300

    Add winner message.
    The winner message has been added,
    along with new functionalities to clear elements
    from the screen.

--- javascript.js
+++ javascript.js
@@ -9,10 +9,14 @@ buttons.forEach((button) => {
     button.addEventListener("click", () => playRound(button.id));
 });
 
-let result = document.querySelector(".results");
+let result = document.querySelector(".results"); 
 let score = document.querySelector(".score");
 let scoreHuman = document.createElement("li");
+score.appendChild(scoreHuman);
 let scoreComputer = document.createElement("li");
+score.appendChild(scoreComputer);
+
+let finalResult = document.querySelector("p");
 
 // By using the Math.random method, we can generate random numbers between 0 and 1 and divide them into predefined intervals. 
 // Each interval corresponds to a specific range of values, representing one of the computer's three possible choices.
@@ -31,16 +35,22 @@ function getComputerChoice(){
 
 let humanScore = 0;
 let computerScore = 0;  
-
+let winner = '';
     
 // Using the user's choice captured by the event listener, we define a function that simulates a round of the game. 
 // This function compares the user's selection with the computer's choice and determines the outcome for that round.
 function playRound(humanSelection){
+    
     if(humanScore == 5 || computerScore == 5){
+        
+        score.removeChild(scoreHuman);
+        score.removeChild(scoreComputer);
+        result.textContent = '';
+        finalResult.textContent = "The winner is " + `${ winner = (humanScore == 5) ? "User" : "Computer"} !!`;
         buttons.forEach((button) => {
             button.removeEventListener("click", () => playRound(button.id));
         });
-        result.textContent = '';
+        
         return;
     }
     else{
@@ -69,9 +79,7 @@ function playRound(humanSelection){
         }
 
         scoreHuman.textContent = "Human Score: " + `${humanScore}`;
-        score.append(scoreHuman);
         scoreComputer.textContent = "Computer Score: " + `${computerScore}`;
-        score.append(scoreComputer);
 
     }
         
